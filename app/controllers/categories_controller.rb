8b597c30f92c2caa8caf2b1cb77b8912dcc64eee
Carey Costa <careyxcosta@gmail.com>
2017-02-27 17:37:56 -0500

    categories, reviews, user, user_sessions controllers updated. user_sessions views created

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,10 @@
 class CategoriesController < ApplicationController
+  def index
+    @categories = Category.all.order('category')
+  end
+
+  def show
+    @category = Category.find(params[:id])
+  end
+  
 end
