1c7d18b14557a2934fbf6662675032a343336036
Carey Costa <careyxcosta@gmail.com>
2017-02-27 17:25:53 -0500

    associations updated

--- app/models/space.rb
+++ app/models/space.rb
@@ -1,5 +1,7 @@
 class Space < ApplicationRecord
   has_and_belongs_to_many :amenities
+  has_and_belongs_to_many :categories
   has_many :reviews
   has_one :location
+  belongs_to :host, :class_name => 'User', :foreign_key => 'host_id'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   has_many :bookings
   has_many :reviews
-  has_many :spaces
+  has_many :spaces, foreign_key: :host_id
 end
