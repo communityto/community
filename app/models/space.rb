4b8c032fe5c999f431ddb26b219e05b24d7a9726
Claire Leslie <c.leslie@live.ca>
2017-02-28 15:16:56 -0500

    merge conflicts solved, rebasing

--- app/models/space.rb
+++ app/models/space.rb
@@ -5,5 +5,6 @@ class Space < ApplicationRecord
   has_many :reviews
   has_one :location
   belongs_to :host, :class_name => 'User', :foreign_key => 'host_id'
+  has_many :bookings
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,11 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates_presence_of :first_name, :last_name
 
+<<<<<<< HEAD
   has_many :reviews, through: :bookings
   has_many :bookings
   has_many :booked_spaces, :class_name => 'Space', through: :bookings, source: :space
   has_many :hosted_spaces, :class_name => 'Space', source: :space, foreign_key: :host_id
+=======
+>>>>>>> add new user form and show page
 end
