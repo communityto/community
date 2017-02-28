bf947639af272f0d344663c8e07fbd39e2c54de8
STEPHANIE <WU.STEPHANIE@LIVE.COM>
2017-02-28 15:18:02 -0500

    Change schema and add validations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,7 @@
 class Booking < ApplicationRecord
+  validates :start_datetime, uniqueness: true
+  validates :end_datetime, uniqueness: true
+
   belongs_to :user
   belongs_to :space
   has_one :review
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,6 @@
 class Review < ApplicationRecord
+  validates_presence_of :accuracy, :communication, :facilities, :location, :content
+
   belongs_to :user
   belongs_to :space
   belongs_to :booking
