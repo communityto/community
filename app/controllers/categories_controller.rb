b92f1ea3c7736cca4c2a1a2a5bdcb5ef5efb38f3
Claire Leslie <c.leslie@live.ca>
2017-02-28 13:44:06 -0500

    add views to categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,10 +1,10 @@
 class CategoriesController < ApplicationController
   def index
-    @categories = Category.all.order('category')
+    @categories = Category.all.order('type')
   end
 
   def show
     @category = Category.find(params[:id])
   end
-  
+
 end
