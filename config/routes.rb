173381b5373acb876d6f3b576aa40c39c17e1d57
Carey Costa <careyxcosta@gmail.com>
2017-03-01 18:52:48 -0500

    able to update a space

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root "spaces#index"
 
+resources :spaces, only: [:edit, :update]
 resources :spaces, only: [:index, :new, :create, :show] do
   resources :bookings
   resources :reviews
@@ -13,6 +14,7 @@ resources :categories, only: [:index, :show]
 
 get 'login' => 'user_sessions#new', :as => :login
 post 'logout' => 'user_sessions#destroy', :as => :logout
+
 
 
 end
