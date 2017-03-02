14cef453ed991baf3c56e71fe440917fdb29371d
EFFY MIN <effymin@gmail.com>
2017-03-02 16:05:37 -0500

    refactored reviews logic into models

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -8,10 +8,7 @@ class SpacesController < ApplicationController
   def show
     @space = Space.find(params[:id])
 
-    @review_accuracy_avg = review_accuracy_avg
-    @review_communication_avg = review_communication_avg
-    @review_facilities_avg = review_facilities_avg
-    @review_location_avg = review_location_avg
+    @reviews = @space.reviews
 
     @review = Review.new
 
@@ -56,34 +53,5 @@ class SpacesController < ApplicationController
   def space_params
     params.require(:space).permit(:title, :description, :address, :check_in, :check_out, :rules, :capacity, :bathrooms, :price, :size, {avatars: []})
   end
-
-  def find_review
-    Review.where(:space_id => (params[:id]))
-  end
-
-  def review_accuracy_avg
-    review_accuracy_sum = find_review.sum(:accuracy)
-    review_accuracy_count = find_review.count(:accuracy)
-    review_accuracy_avg = review_accuracy_sum / review_accuracy_count
-  end
-
-  def review_communication_avg
-    review_communication_sum = find_review.sum(:communication)
-    review_communication_count = find_review.count(:communication)
-    review_communication_avg = review_communication_sum / review_communication_count
-  end
-
-  def review_facilities_avg
-    review_facilities_sum = find_review.sum(:facilities)
-    review_facilities_count = find_review.count(:facilities)
-    review_facilities_avg = review_facilities_sum / review_facilities_count
-  end
-
-  def review_location_avg
-    review_location_sum = find_review.sum(:location)
-    review_location_count = find_review.count(:location)
-    review_location_avg = review_location_sum / review_location_count
-  end
-
 
 end
