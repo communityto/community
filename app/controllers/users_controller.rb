50927597845221e163ee58df265ac061d8f9a26d
Claire Leslie <c.leslie@live.ca>
2017-03-02 15:19:43 -0500

    add user instance variable to user show, add links to individual space pages in index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,25 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
-  skip_before_action :require_login, only: [:index, :new, :create]
+  # before_action :set_user, only: [:show, :edit, :update, :destroy]
+  # skip_before_action :require_login, only: [:index, :new, :create]
 
   # GET /users
   # GET /users.json
   def index
-
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   # GET /users/1
   # GET /users/1.json
   def show
+    @user = current_user
   end
 
   # GET /users/new
