a534ac3d3aae863020385bbf86eb20e803725eef
STEPHANIE <WU.STEPHANIE@LIVE.COM>
2017-03-05 20:42:58 -0500

    Create search controller, model and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
 
-  resources :favourite_spaces, only: [:create, :destroy]
-
+  # HOME
   root "spaces#index"
+
+  # USERS
   resources :user_sessions, only: [:create]
   resources :users, only: [:new, :create, :show, :edit] do
     resources :bookings, only: [:index, :edit, :update, :destroy]
@@ -10,14 +11,25 @@ Rails.application.routes.draw do
     resources :favourite_spaces, only: [:index]
     resources :hosted_spaces, only: [:index, :show]
   end
+
+  # ACCOUNTS
   get 'login' => 'user_sessions#new', :as => :login
   post 'logout' => 'user_sessions#destroy', :as => :logout
 
+  # SPACES
   resources :spaces do
     resources :bookings
     resources :reviews
   end
 
+  # FAVOURITES
+  resources :favourite_spaces, only: [:create, :destroy]
+
+  # CATEGORIES
   resources :categories, only: [:index, :show]
+
+  # SEARCH
+  get 'search', to: 'search#new'
+  get 'search_results', to: 'search#results'
 
 end
