3a5933881be19e7d78330383f2ae3056cccd047b
EFFY MIN <effymin@gmail.com>
2017-03-09 23:49:09 -0500

    change star render logic to check for fixnum

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,23 +1,28 @@
 module ApplicationHelper
 
   # def render_stars(value)
-  #    output = ''
-  #    if (1..5).include?(value.to_i)
-  #       value.to_i.times { output += '*'}
-  #    end
-  #    output
+  #   output = ''
+  #   if (1..5).include?(value.floor)
+  #     value.floor.times { output += image_tag('stars.png')}
+  #   end
+  #   if value == (value.floor + 0.5) && value.to_i != 5
+  #     output += image_tag('half_star.png')
+  #   end
+  #   output.html_safe
   # end
 
   def render_stars(value)
     output = ''
-      if value.class == Integer && (1..5).include?(value.floor)
+      if value.class == Fixnum && (1..5).include?(value.floor)
         value.floor.times { output += image_tag('stars.png')}
       else
         return value
       end
 
       # if value == (value.floor + 0.5) && value.to_i != 5
-      #   output += image_tag('half_star.png')
+      #   output += image_tag('stars.png')
+      # else
+      #   return value
       # end
     output.html_safe
     end
