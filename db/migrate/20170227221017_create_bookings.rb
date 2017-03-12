8fe81c5f2f13254bb9032787dd74b6ada9e58378
STEPHANIE <WU.STEPHANIE@LIVE.COM>
2017-03-11 19:24:02 -0500

    Install simple calendar gem and render calendar view of bookings

--- db/migrate/20170227221017_create_bookings.rb
+++ db/migrate/20170227221017_create_bookings.rb
@@ -3,8 +3,8 @@ class CreateBookings < ActiveRecord::Migration[5.0]
     create_table :bookings do |t|
       t.integer :user_id
       t.integer :space_id
-      t.datetime :start_datetime
-      t.datetime :end_datetime
+      t.datetime :start_time
+      t.datetime :end_time
       t.text :note
 
       t.timestamps
