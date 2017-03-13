656840f926ef51e26801436de18b0c180adc5c8b
Claire Leslie <c.leslie@live.ca>
2017-03-13 14:33:11 -0400

    add reply route, fix links in messaging views and controllers

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -8,16 +8,29 @@ class ConversationsController < ApplicationController
   end
 
   def show
-    @personal_message = PersonalMessage.new
+    @personal_message = current_user.personal_messages.build
+  end
+
+  def reply
+    @personal_message = current_user.personal_messages.build(personal_message_params)
+    @personal_message.conversation_id = @conversation.id
+    @personal_message.save!
+
+    flash[:success] = "Your message was sent!"
+    redirect_to user_conversation_path(current_user, @conversation)
   end
 
   private
 
   def set_conversation
-    @conversation = Contion.find_by(id: params[:id])
+    @conversation = Conversation.find(params[:id])
   end
 
   def check_participating!
     redirect_to root_path unless @conversation && @conversation.participates?(current_user)
+  end
+
+  def personal_message_params
+    params.require(:personal_message).permit(:body)
   end
 end
