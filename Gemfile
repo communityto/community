4e3ca9a1113107ad5c445c9a5aaa6c6d4d91fe30
Carey Costa <careyxcosta@gmail.com>
2017-03-13 16:01:31 -0400

    added geocoder gem to gemfile, added longitude and latitude to addresses in schema

--- Gemfile
+++ Gemfile
@@ -61,6 +61,8 @@ gem 'figaro'
 
 gem 'simple_calendar', '~> 2.0'
 
+gem 'geocoder'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170312000238) do
+ActiveRecord::Schema.define(version: 20170313195906) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20170312000238) do
     t.string  "street_address"
     t.string  "neighbourhood"
     t.integer "space_id"
+    t.float   "latitude"
+    t.float   "longitude"
   end
 
   create_table "amenities", force: :cascade do |t|
