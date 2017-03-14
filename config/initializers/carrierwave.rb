49526403d8ed3aa54221d9cb48a8aa5840239978
Claire Leslie <c.leslie@live.ca>
2017-03-14 13:09:42 -0400

    require emoji gem in stylesheet and applicationjs, make text fields in messenger portion of application emojiable

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,6 +5,6 @@ CarrierWave.configure do |config|
     aws_access_key_id: Figaro.env.aws_access_key_id!,
     aws_secret_access_key: Figaro.env.aws_secret_access_key!
   }
-  config.fog_directory  = 'community.toronto'
+  config.fog_directory  = 'communitytoronto'
   config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
 end
