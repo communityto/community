33b59d580165a36e2cf3fcb1ca84eb164e8050f2
Carey Costa <careyxcosta@gmail.com>
2017-03-14 18:25:24 -0400

    addresses now appear on a map of toront

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,11 +1,11 @@
 class Address < ApplicationRecord
   geocoded_by :full_address
-  after_validation :geocode
+  # after_validation :geocode, if: ->(obj){ obj.street_address.present? and obj.street_address_changed? }
 
   belongs_to :space
 
   def full_address
-    [:street_address, :city, :province].compact.join(', ')
+    [street_address, city, province].compact.join(', ')
   end
 
 end
