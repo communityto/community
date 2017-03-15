b3e08fb9d3fff0af3f19947992333845cb0e5684
STEPHANIE <WU.STEPHANIE@LIVE.COM>
2017-03-15 17:29:28 -0400

    Fixed jQuery on calendar view of spaces

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,7 +6,7 @@ class SearchController < ApplicationController
 
   private
   def search_params
-    params.require(:search).permit(:capacity, :amenity_ids => [], :category_ids => [])
+    params.require(:search).permit(:start_time, :end_time, :capacity, :amenity_ids => [], :category_ids => [])
   end
 
 end
