2efde0835c3751f4dda5f3eb604be3631c3b3d5b
Claire Leslie <c.leslie@live.ca>
2017-03-16 16:35:21 -0400

    add delete for hosted spaces

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 
-ActiveRecord::Schema.define(version: 20170314023236) do
 
 ActiveRecord::Schema.define(version: 20170311214020) do
 
