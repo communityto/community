110267b47db9d20a34d5780ad0aa97f52dd50840
STEPHANIE <WU.STEPHANIE@LIVE.COM>
2017-03-16 17:47:01 -0400

    Add Facebook API keys to figaro and application.yml

--- config/initializers/figaro.rb
+++ config/initializers/figaro.rb
@@ -1,1 +1,1 @@
-Figaro.require_keys("aws_access_key_id", "aws_secret_access_key")
+Figaro.require_keys("aws_access_key_id", "aws_secret_access_key", "facebook_access_key_id", "facebook_secret_access_key")
