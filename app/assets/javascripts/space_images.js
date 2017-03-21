cb37cb8b3a41e1b29028afb64fa3e59e24d78344
Carey Costa <careyxcosta@gmail.com>
2017-03-21 13:15:16 -0400

    images display in section on space show page

--- app/assets/javascripts/space_images.js
+++ app/assets/javascripts/space_images.js
@@ -1,2 +1,9 @@
 // Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+// All this logic will automatically be available in application.js
+
+$(document).ready(function() {
+  $("#space-photo").click(function(e) {
+       e.preventDefault();
+       $("#popup").html( $('<%= p.photo %>').attr('src', this.href) );
+    });
+});
