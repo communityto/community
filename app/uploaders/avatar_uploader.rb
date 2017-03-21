099eb2d6043e5b636ae23233cfd64571995071f8
STEPHANIE <WU.STEPHANIE@LIVE.COM>
2017-03-21 14:52:53 -0400

    Reinstate lines that allow multi-photo uploads

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,9 +1,5 @@
 class AvatarUploader < CarrierWave::Uploader::Base
 
-  # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
-  include CarrierWave::MiniMagick
-
   # Choose what kind of storage to use for this uploader:
   # storage :file
   storage :fog
@@ -28,17 +24,6 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # def scale(width, height)
   #   # do something
   # end
-
-  # Create different versions of your uploaded files:
-  version :thumb do
-    process resize_to_fit: [100, 100]
-  end
-
-  # Add a white list of extensions which are allowed to be uploaded.
-  # For images you might use something like this:
-  def extension_whitelist
-    %w(jpg jpeg gif png)
-  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
